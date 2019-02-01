aa771e839713dca2c5d99367d3c2db60e5f607b5
sdemianets <stanislav.demianets@amadeus.com>
2019-02-01 08:41:55 +0100

    adding draft gulp runner

--- scenarioList.js
+++ scenarioList.js
@@ -1,5 +1,5 @@
 var buildFilePathE2ESanity = function (scenarioName) {
-  return './generated/flows/e2eSanity/' + scenarioName + '.js';
+  return './generated/flows/e2eSanity/' + scenarioName + '.js'; // ../generated/flows/e2eSanity/' + scenarioName + '.js'; // move outside of scripts folder in case of gulp
 };
 
 exports.e2eSanity = [
---
+++ scripts/gulpfile.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var shell = require('gulp-shell');
+var protractor = require("gulp-protractor").protractor;
+var reporterUtils;
+
+
+// Delete previous generated folder
+gulp.task('clean', function () {
+  return gulp.src('../generated', {read: false, allowEmpty: true})
+    .pipe(clean({force: true}));
+});
+
+// transpile js from ts files
+gulp.task('tsc', shell.task('tsc'));
+
+gulp.task('build', gulp.series('clean', 'tsc'));
+
+// run protractor tests
+gulp.task('prot', function(callback) {
+  var configFile = require('../qaprotractor.conf.js');
+
+  gulp.src(configFile.config.specs)
+    .pipe(protractor({
+      configFile: "../qaprotractor.conf.js",
+      args: [
+        '--baseUrl', 'http://localhost:4444/wd/hub',
+      ]
+    }))
+    .on('error', function(e) { throw e })
+});
+
+// this is task to Global HTML reports.
+gulp.task('report', function () {
+  setTimeout(function () {
+    reporterUtils = require('../generated/fwk/reportUtils/ReporterUtils');
+    reporterUtils.generateGlobalReports();
+  }, 5000);
+});
+
+gulp.task('e2e', gulp.series('clean', 'tsc', 'prot', 'report'));
