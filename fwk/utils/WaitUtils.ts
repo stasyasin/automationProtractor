18c08ac6a2a0bcbc6f7e16c03b1aea71be596b7f
Stanislav <stasyasin@gmail.com>
2023-08-31 15:53:02 +0200

    Update WaitUtils.ts

--- fwk/utils/WaitUtils.ts
+++ fwk/utils/WaitUtils.ts
@@ -4,45 +4,53 @@ import protractor = require('protractor');
 const globalWaitTimeout: number = 40 * 1000;
 
 export async function waitForElementNotPresent(elem: ElementFinder, timeoutSec?: number): Promise<void> {
-  await browser.wait(protractor.ExpectedConditions.not(protractor.ExpectedConditions.presenceOf(elem)), getTimeout(timeoutSec),
-    `${elem.locator()} is present after ${timeoutSec} sec`)
+  const timeoutMilliSec: number = getTimeout(timeoutSec);
+  await browser.wait(protractor.ExpectedConditions.not(protractor.ExpectedConditions.presenceOf(elem)), timeoutMilliSec,
+    `${elem.locator()} is present after ${timeoutMilliSec/1000} sec`)
     .then(() => {})
     .catch(() => {
-      console.log(`${elem.locator()} is present after ${timeoutSec} sec`);
+      console.log(`${elem.locator()} is present after ${timeoutMilliSec/1000} sec`);
     });
 }
 
 export async function waitForElementPresent(elem: ElementFinder, timeoutSec?: number): Promise<void> {
-  await browser.wait(protractor.ExpectedConditions.presenceOf(elem), getTimeout(timeoutSec),
-    `${elem.locator()} is not present after ${timeoutSec} sec`)
+  const timeoutMilliSec: number = getTimeout(timeoutSec);
+  await browser.wait(protractor.ExpectedConditions.presenceOf(elem), timeoutMilliSec,
+    `${elem.locator()} is not present after ${timeoutMilliSec/1000} sec`)
     .then(() => {})
     .catch(() => {
-      console.log(`${elem.locator()} is not present after ${timeoutSec} sec`);
+      console.log(`${elem.locator()} is not present after ${timeoutMilliSec/1000} sec`);
     });
 }
 
 export async function waitForElementPresentAndVisible(elem: ElementFinder, timeoutSec?: number): Promise<void> {
   await this.waitForElementPresent(elem, timeoutSec);
-  await browser.wait(protractor.ExpectedConditions.visibilityOf(elem), getTimeout(timeoutSec),
-    `${elem.locator()} is not visible after ${timeoutSec} sec`).then(() => {
-  });
+  const timeoutMilliSec: number = getTimeout(timeoutSec);
+  await browser.wait(protractor.ExpectedConditions.visibilityOf(elem), getTimeout(timeoutMilliSec),
+    `${elem.locator()} is not visible after ${timeoutMilliSec/1000} sec`)
+    .then(() => {})
+    .catch(() => {
+        console.log(`${elem.locator()} is not visible after ${timeoutMilliSec/1000} sec`);
+      });
 }
 
 export async function waitForTextPresentInElement(elem: ElementFinder, text: string, timeoutSec?: number): Promise<void> {
-  await browser.wait(protractor.ExpectedConditions.textToBePresentInElement(elem, text), getTimeout(timeoutSec),
-    `${text} is not present in ${elem.locator()} after ${timeoutSec} sec`)
+  const timeoutMilliSec: number = getTimeout(timeoutSec);
+  await browser.wait(protractor.ExpectedConditions.textToBePresentInElement(elem, text), timeoutMilliSec,
+    `${text} is not present in ${elem.locator()} after ${timeoutMilliSec/1000} sec`)
     .then(() => {})
     .catch(() => {
-        console.log(`${elem.locator()} is not visible after ${timeoutSec} sec`);
+        console.log(`${elem.locator()} is not visible after ${timeoutSec/1000} sec`);
       });
 }
 
 export async function waitForButtonToBeClickable(elem: ElementFinder, timeoutSec?: number): Promise<void> {
-  await browser.wait(protractor.ExpectedConditions.elementToBeClickable(elem), getTimeout(timeoutSec),
-    `${elem.locator()} is not clickable after ${timeoutSec} sec`)
+  const timeoutMilliSec: number = getTimeout(timeoutSec);
+  await browser.wait(protractor.ExpectedConditions.elementToBeClickable(elem), timeoutMilliSec,
+    `${elem.locator()} is not clickable after ${timeoutMilliSec/1000} sec`)
     .then(() => {})
     .catch(() => {
-      console.log(`${text} is not present in ${elem.locator()} after ${timeoutSec} sec`);
+      console.log(`${text} is not present in ${elem.locator()} after ${timeoutMilliSec/1000} sec`);
     });
 }
 
